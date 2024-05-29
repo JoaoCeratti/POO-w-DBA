e65b98e7d993e57ab2f254022ec28397827c0381
João Ceratti <160682434+JoaoCeratti@users.noreply.github.com>
2024-05-28 21:36:14 -0300

    Add files via upload

---
+++ erp/src/main/java/br/unipar/erp/data/entity/Usuario.java
@@ -0,0 +1,22 @@
+package br.unipar.erp.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+
+public class Usuario {
+
+    @Id
+
+    private Integer codigo;
+
+    private String usuario;
+
+    private String senha;
+
+    private LocalDate dataNascimento;
+}
+
